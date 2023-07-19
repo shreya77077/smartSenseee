28720a5426a0062ee1a05a8e488fe22fd9b6209a
Shreya Gupta <shreya.gupta13606@gmail.com>
2023-07-19 12:54:10 +0530

    init

--- Todos.sol
+++ Todos.sol
@@ -8,36 +8,29 @@ contract TodoList {
         uint256 deadline;
     }
 
-   
-    Todo[] public todos;
+    mapping(address => mapping(uint => Todo[])) public User;
 
-     mapping(address => mapping(uint => Todo)) public User;
-
-    function userstore(string calldata _things, uint _deadline, uint id) public {
-        User[msg.sender][id] = Todo(_things,false,_deadline);
+    function userstore(string calldata _things, uint256 _deadline, uint id) public {
+        User[msg.sender][id].push(Todo(_things, false, block.timestamp + _deadline));
     }
 
-    // function create(string calldata _things, uint256 _deadline) public {
-    //     todos.push(Todo(_things, false, block.timestamp + _deadline));
-    // }
-
-    function updateThings(uint256 _index, string calldata _things) public {
-        todos[_index].things = _things;
+    function updateThings(uint256 _index, string calldata _things, uint id) public {
+        User[msg.sender][id][_index].things = _things;
     }
 
-    function toggleCompleted(uint256 _index) external {
-        require(!todos[_index].completed, "Already completed");
-        require(block.timestamp < todos[_index].deadline, "Deadline passed");
-        todos[_index].completed = true;
+    function toggleCompleted(uint256 _index, uint id) external {
+        Todo storage todo = User[msg.sender][id][_index];
+        require(!todo.completed, "Already completed");
+        require(block.timestamp < todo.deadline, "Deadline passed");
+        todo.completed = true;
     }
 
-    function remove(uint256 _index) public returns (Todo[] memory) {
-        require(_index < todos.length, "Invalid index");
-
-        for (uint256 i = _index; i < todos.length - 1; i++) {
-            todos[i] = todos[i + 1];
+    function remove(uint256 _index, uint id) public returns (Todo[] memory) {
+         for (uint256 i = _index; i < User[msg.sender][id].length - 1; i++) {
+            User[msg.sender][id][i] = User[msg.sender][id][i + 1];
         }
-        todos.pop();
-        return todos;
+        User[msg.sender][id].pop();
+
+        return User[msg.sender][id];
     }
 }
